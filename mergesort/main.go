78c80a01e70b497893fbb57d708063400737582b
bradkibs <bradleykibs@gmail.com>
2024-11-17 05:43:49 +0300

    creating a detailed display

--- mergesort/main.go
+++ mergesort/main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	newList := [14]int{21, 1, 3, 2, 45, 12, 67, 5, 4, 3, 87, 76, 12, 1}
+	fmt.Println("-------------List before merge sort--------------------")
+	fmt.Println(newList)
 	sliceOfNewList := newList[:]
+	fmt.Println("--------------The mergesort algorithm visualization-------------")
 	mergeSort(&sliceOfNewList, 0, 13)
+
+	fmt.Println("---------The list After merge sort------------")
 	fmt.Println(sliceOfNewList)
 }
